272f7f71e67f0ec2b5b80728ea46aef878ced75c
Inno97 <daniel-tan2009@hotmail.com>
2022-03-06 18:04:17 +0800

    Update sql files in section 2 for better examples

--- s2_databases/sql/query2.sql
+++ s2_databases/sql/query2.sql
@@ -5,14 +5,7 @@ SELECT car.Manufacturer as Manufacturer, Count(car.Manufacturer) as Sales_Count,
 FROM dealership.Transaction transaction
 INNER JOIN dealership.Car as car ON Car.Id = transaction.CarId
 WHERE EXTRACT(YEAR FROM transaction.Date) = 2022 AND
-EXTRACT(MONTH FROM transaction.Date) = 1
+EXTRACT(MONTH FROM transaction.Date) = 3
 GROUP BY car.Manufacturer
 ORDER BY Count(car.Manufacturer) DESC
 FETCH FIRST 3 ROWS ONLY
-
--- NOTE: we can replace EXTRACT(MONTH FROM transaction.Date) = 1 by 
--- EXTRACT(MONTH FROM transaction.Date) = EXTRACT(MONTH FROM CURRENT_DATE)
--- and EXTRACT(YEAR FROM transaction.Date) = 2022 by
--- EXTRACT(YEAR FROM transaction.Date) = EXTRACT(YEAR FROM CURRENT_DATE)
--- to set to the current month. But since this can be run in the future, there
--- will be a case where nothing is fetched since transactions are not within this month
